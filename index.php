8c93e48dc1087afff07ba5903d90b0fbac1d69a0
vikrantghan <vikrantghan@gmail.com>
2015-11-18 13:18:17 +0530

    dd
    dd

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+sdfdsfds		$uri = 'https://';
+	} else {dsfdsf
+		$uri = 'http://';
+	}sdffds
+	$uri .= $_SERVER['HTTP_HOST'];
+	header('Location: '.$uri.'/xampp/');
+	exit;
+        dsfdsfds
+?>
+Something is wrong with the XAMPP installation :-(
